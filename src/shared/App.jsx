d22ebd64f07d59d66804b467050e7f9a2eef1c72
hien-alex <hiennnalex@gmail.com>
2021-05-31 10:47:52 -0700

    setting up page from default react

---
+++ src/shared/App.jsx
@@ -0,0 +1,8 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { MainPage } from "../Pages/Main";
+
+export default class App extends Component {
+  render() {
+    <MainPage />;
+  }
+}
