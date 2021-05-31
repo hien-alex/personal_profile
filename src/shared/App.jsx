9e19ca3fc3eeaa47116c63b81055e1a15841c408
hien-alex <hiennnalex@gmail.com>
2021-05-31 11:18:40 -0700

    initial setup complete

--- src/shared/App.jsx
+++ src/shared/App.jsx
@@ -1,8 +1,13 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import { MainPage } from "../Pages/Main";
+import MainPage from "../Pages/Main";
+import React, { Component } from "react";
 
 export default class App extends Component {
   render() {
-    <MainPage />;
+    return (
+      <div style={{ padding: "4vh" }}>
+        <Route path="/" component={MainPage}></Route>
+      </div>
+    );
   }
 }
