e8eb4cd8dd2cd99f35f4a7d0f48c3c840314d876
hien-alex <hiennnalex@gmail.com>
2021-06-01 11:51:12 -0700

    mini changes, testing how things work

--- src/Pages/Main.js
+++ src/Pages/Main.js
@@ -1,5 +1,32 @@
 import React from "react";
+import Container from "@material-ui/core/Container";
+import Typography from "@material-ui/core/Typography";
 
 export default function MainPage() {
-  return <h1>Hello! Welcome to my webpage. I am Alex Hien.</h1>;
+  return (
+    <Container maxWidth="md">
+      <Typography
+        component="div"
+        style={{ backgroundColor: "#cfe8fc", height: "100vh" }}
+      >
+        Hello. I am alex
+      </Typography>
+    </Container>
+  );
 }
+
+// import React from 'react';
+// import CssBaseline from '@material-ui/core/CssBaseline';
+// import Typography from '@material-ui/core/Typography';
+// import Container from '@material-ui/core/Container';
+
+// export default function SimpleContainer() {
+//   return (
+//     <React.Fragment>
+//       <CssBaseline />
+//       <Container maxWidth="sm">
+//         <Typography component="div" style={{ backgroundColor: '#cfe8fc', height: '100vh' }} />
+//       </Container>
+//     </React.Fragment>
+//   );
+// }
