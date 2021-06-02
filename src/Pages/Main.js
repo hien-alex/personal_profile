b388b4b2ac0cc4e09e4019214e141a5da0c4d694
hien-alex <hiennnalex@gmail.com>
2021-06-01 18:49:44 -0700

    new navbar style

--- src/Pages/Main.js
+++ src/Pages/Main.js
@@ -2,48 +2,44 @@ import React from "react";
 import { useSpring, animated } from "react-spring";
 import Container from "@material-ui/core/Container";
 import Typography from "@material-ui/core/Typography";
+import "../css/main.css";
 import { Spring } from "react-spring";
 
 export default function MainPage() {
   return (
-    // <Spring from={{ opacity: 0 }} to={{ opacity: 1 }}>
-    //   {(props) => (
-    //     <div
-    //       style={{
-    //         height: 100,
-    //         width: 100,
-    //         backgroundColor: "pink",
-    //         color: "#fff",
-    //         ...props,
-    //       }}
-    //     >
-    //       1
-    //     </div>
-    //   )}
-    // </Spring>
-    <Container maxWidth="md">
-      <Typography
+    <body>
+      <Container maxWidth="md">
+        {/* <Typography
         component="div"
         style={{ backgroundColor: "#cfe8fc", height: "100vh" }}
       >
         Hello. I am alex
-      </Typography>
-    </Container>
+      </Typography> */}
+        <h1>hey</h1>
+        <h1>hey</h1>
+        <h1>hey</h1>
+        <h1>hey</h1>
+        <h1>hey</h1>
+        <h1>hey</h1>
+        <h1>hey</h1>
+        <h1>hey</h1>
+        <h1>hey</h1>
+        <h1>hey</h1>
+        <h1>hey</h1>
+        <h1>hey</h1>
+        <h1>hey</h1>
+        <h1>hey</h1>
+        <h1>hey</h1>
+        <h1>hey</h1>
+        <h1>hey</h1>
+        <h1>hey</h1>
+        <h1>hey</h1>
+        <h1>hey</h1>
+        <h1>hey</h1>
+        <h1>hey</h1>
+        <h1>hey</h1>
+        <h1>hey</h1>
+      </Container>
+    </body>
   );
 }
-
-// import React from 'react';
-// import CssBaseline from '@material-ui/core/CssBaseline';
-// import Typography from '@material-ui/core/Typography';
-// import Container from '@material-ui/core/Container';
-
-// export default function SimpleContainer() {
-//   return (
-//     <React.Fragment>
-//       <CssBaseline />
-//       <Container maxWidth="sm">
-//         <Typography component="div" style={{ backgroundColor: '#cfe8fc', height: '100vh' }} />
-//       </Container>
-//     </React.Fragment>
-//   );
-// }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,55 +6,98 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { Link } from "react-router-dom";
 import { useSpring, animated } from "react-spring";
 import { Spring } from "react-spring";
+import styled, { css } from "styled-components/macro";
 
 function MyNavBar() {
-  const fade = useSpring({
-    config: { duration: 1000 },
-    from: { opacity: 0 },
-    opacity: 1,
-  });
-  const fade1 = useSpring({
-    config: { duration: 1500 },
-    from: { opacity: 0 },
-    opacity: 1,
-  });
-  const fade2 = useSpring({
-    config: { duration: 2000 },
-    from: { opacity: 0 },
-    opacity: 1,
-  });
-  const fade3 = useSpring({
-    config: { duration: 2500 },
-    from: { opacity: 0 },
-    opacity: 1,
-  });
+  // const fade = useSpring({
+  //   config: { duration: 1000 },
+  //   from: { opacity: 0 },
+  //   opacity: 1,
+  // });
+  // const fade1 = useSpring({
+  //   config: { duration: 1500 },
+  //   from: { opacity: 0 },
+  //   opacity: 1,
+  // });
+  // const fade2 = useSpring({
+  //   config: { duration: 2000 },
+  //   from: { opacity: 0 },
+  //   opacity: 1,
+  // });
+  // const fade3 = useSpring({
+  //   config: { duration: 2300 },
+  //   from: { opacity: 0 },
+  //   opacity: 1,
+  // });
+
+  const Nav = styled.nav`
+    height: 60px;
+    background: #eacda3;
+    background: -webkit-linear-gradient(to right, #d6ae7b, #eacda3);
+    background: linear-gradient(to right, #d6ae7b, #eacda3);
+    display: flex;
+    justify-content: space-between;
+    padding: 1rem 2rem;
+    z-index: 100;
+    position: sticky;
+    width: 100%;
+    top: 0;
+  `;
+
+  const NavLink = css`
+    color: white;
+    display: flex;
+    align-items: center;
+    padding: 0 1rem;
+    height: 100%;
+    cursor: pointer;
+    text-decoration: none;
+
+    &:hover {
+      transform: translateY(-2px);
+      color: white;
+      text-decoration: none;
+    }
+  `;
+
+  const Logo = styled(Link)`
+    ${NavLink}
+    padding: 0;
+    font-style: italic;
+  `;
+
+  const MenuBars = styled.i``;
+
+  const NavMenu = styled.div`
+    display: flex;
+    align-items: center;
+    text-align: center;
+  `;
+
+  const NavMenuLinks = styled(Link)`
+    ${NavLink}
+  `;
+
+  const menuData = [
+    { title: "Home", link: "/" },
+    { title: "Contact", link: "/contact" },
+    { title: "About", link: "/aboutme" },
+    { title: "Projects", link: "/projects" },
+  ];
 
   return (
-    <Container className="p-0" fluid={true}>
-      <Navbar className="border-bottom" expand="lg">
-        <Navbar.Brand>
-          <animated.div style={fade}>Alex Hien</animated.div>
-        </Navbar.Brand>
-
-        <Navbar.Toggle aria-controls="navbar-toggle" className="border-0" />
-        <Navbar.Collapse>
-          <Nav className="ml-auto">
-            <Link className="nav-link" to="/" style={fade}>
-              <animated.div style={fade}>Home</animated.div>
-            </Link>
-            <Link className="nav-link" to="/projects">
-              <animated.div style={fade1}>My Projects</animated.div>
-            </Link>
-            <Link className="nav-link" to="aboutme">
-              <animated.div style={fade2}>About Me</animated.div>
-            </Link>
-            <Link className="nav-link" to="contact">
-              <animated.div style={fade3}>Contact Me</animated.div>
-            </Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
-    </Container>
+    <Nav>
+      {/* <Logo to="/">Alex Hien</Logo> */}
+      <MenuBars />
+      <NavMenu>
+        {menuData.map((item, index) => (
+          <NavMenuLinks to={item.link} key={index}>
+            {item.title}
+          </NavMenuLinks>
+        ))}
+      </NavMenu>
+      <div />
+    </Nav>
   );
 }
 
