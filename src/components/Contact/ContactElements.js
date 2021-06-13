2a67c8e23fe3a636d5a2bc7c7ee2ff1ddd42b3d3
alex-hien <hiennalex@gmail.com>
2021-06-13 16:28:57 -0700

    skeleton contact page

--- src/components/Contact/ContactElements.js
+++ src/components/Contact/ContactElements.js
@@ -1,10 +1,13 @@
 import styled from "styled-components";
+import Form from "react-bootstrap/Form";
+import { Button } from "@material-ui/core";
+import "../../css/Contact.css";
 
 export const ContactBackground = styled.div`
   height: 90vh;
   top: 0;
   width: 100%;
-  background: grey;
+  background: #e5e5e5;
   position: absolute;
 `;
 
@@ -18,9 +21,39 @@ export const ContactContent = styled.div`
   justify-content: center;
   text-align: center;
   position: absolute;
-  display: inline;
+  display: grid;
   flex-direction: row;
   align-items: center;
-  width: 90%;
-  left: 5%;
+  justify-items: center;
+  width: 100%;
+  margin-top: 4vh;
 `;
+
+export const ContactForm = () => {
+  return (
+    <Form className="mainCol">
+      <Form.Group className="text-left">
+        <label style={{ fontSize: "6rem" }}>Let's talk</label>
+        <div className="formInputs">
+          <label style={{ fontSize: "2rem" }}>Full Name</label>
+          <Form.Control type="text" placeholder="Full Name"></Form.Control>
+        </div>
+        <div className="formInputs">
+          <label style={{ fontSize: "2rem" }}>Email</label>
+          <Form.Control type="email" placeholder="Email Address"></Form.Control>
+        </div>
+        <div className="formInputs">
+          <label style={{ fontSize: "2rem" }}>Message</label>
+          <Form.Control
+            as="textarea"
+            rows="4"
+            placeholder="Enter Message..."
+          ></Form.Control>
+        </div>
+      </Form.Group>
+      <Button size="large" color="inherit" variant="text" disabled={false}>
+        Send
+      </Button>
+    </Form>
+  );
+};
--- src/components/Contact/ContactSection.js
+++ src/components/Contact/ContactSection.js
@@ -2,36 +2,16 @@ import {
   ContactBackground,
   ContactContainer,
   ContactContent,
+  ContactForm,
 } from "./ContactElements";
 import Form from "react-bootstrap/Form";
 
 const ContactSection = () => {
   return (
-    <ContactContainer>
+    <ContactContainer id="contact">
       <ContactBackground />
       <ContactContent>
-        <Form>
-          <div className="mb-3">
-            <Form.File id="formcheck-api-custom" custom>
-              <Form.File.Input isValid />
-              <Form.File.Label
-                data-browse="Button text"
-                style={{ color: "#fec89a" }}
-              >
-                Custom file input
-              </Form.File.Label>
-              <Form.Control.Feedback type="valid">
-                You did it!
-              </Form.Control.Feedback>
-            </Form.File>
-          </div>
-          <div className="mb-3">
-            <Form.File id="formcheck-api-regular">
-              <Form.File.Label>Regular file input</Form.File.Label>
-              <Form.File.Input />
-            </Form.File>
-          </div>
-        </Form>
+        <ContactForm />
       </ContactContent>
     </ContactContainer>
   );
