34f28692c0aefdd70e892be1390c1c86ab0cb0d6
hien-alex <hiennnalex@gmail.com>
2021-12-14 23:02:11 -0800

    websiteUpdates

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -9,9 +9,7 @@ import {
   AboutIMG,
 } from "./AboutElements";
 import "../../css/About.css";
-import Snoopy_Peanuts from "../../Images/Snoopy_Peanuts.png";
 import me from "../../Images/me.JPG";
-import { useSpring, animated } from "react-spring";
 
 const About = () => {
   return (
--- src/components/Projects/ProjectsSection.js
+++ src/components/Projects/ProjectsSection.js
@@ -1,15 +1,66 @@
 import { ProjectsContainer, ProjectsCards } from "./ProjectsElements";
 import pathFinderImg from "../../Images/pathfinder.png";
+import meImg from "../../Images/me.JPG";
+import Carousel from "react-bootstrap/Carousel";
+import "../../css/ProjectsSection.css";
 
 const ProjectsSection = () => {
   return (
-    <ProjectsContainer id="projects">
-      <ProjectsCards
-        projectImage={pathFinderImg}
-        projectLink={"https://ahien-pathfinder-project.netlify.app/"}
-        projectName={"Visual Pathfinder Project"}
-      />
-    </ProjectsContainer>
+    // <ProjectsContainer id="projects">
+    //   {/* <ProjectsCards
+    //     projectImage={pathFinderImg}
+    //     projectLink={"https://ahien-pathfinder-project.netlify.app/"}
+    //     projectName={"Visual Pathfinder Project"}
+    //   />
+    //   <ProjectsCards
+    //     projectImage={pathFinderImg}
+    //     projectLink={"https://ahien-pathfinder-project.netlify.app/"}
+    //     projectName={"Visual Pathfinder Project"}
+    //   /> */}
+    // </ProjectsContainer>
+
+    <Carousel pause="hover" fade className="courselCSS">
+      <Carousel.Item interval={5000}>
+        <a href="https://ahien-pathfinder-project.netlify.app/" target="_blank">
+          <img
+            className="d-block w-100"
+            className="carouselItem"
+            src={pathFinderImg}
+            alt="First slide"
+          />
+        </a>
+        <Carousel.Caption className="caption">
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item interval={5000}>
+        <img
+          className="d-block w-100"
+          className="carouselItem"
+          src={meImg}
+          alt="Second slide"
+        />
+        <Carousel.Caption className="caption">
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item interval={5000}>
+        <img
+          className="d-block w-100"
+          className="carouselItem"
+          src={pathFinderImg}
+          alt="Third slide"
+        />
+        <Carousel.Caption className="caption">
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
   );
 };
 
